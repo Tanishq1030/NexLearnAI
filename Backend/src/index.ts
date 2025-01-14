3a46de0aed10c6d9079da931f91323a4b448b44f
Roni bhakta <ronibhakta1@gmail.com>
2025-01-15 01:13:04 +0530

    loginpage edit

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -3,7 +3,9 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  // return c.text('Hello Hono!')
+  return c.html('<h1>SOO JA SNEHAL GANDU</h1>')
+  
 })
 
 export default app
