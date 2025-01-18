3208521719d11c41b018be0139315daa5fe2b6db
ronibhakta1 <ronibhakta1@gmail.com>
2025-01-18 14:49:55 +0000

    idx commit

--- Frontend/src/components/homePage.tsx
+++ Frontend/src/components/homePage.tsx
@@ -5,7 +5,7 @@ import { SparklesPreview } from "./subcomponents/sparklesPreview";
 
 export function HomePage() {
     const navigate = useNavigate();
-    return (<div>
+    return (<div className="bg-black">
         <nav className="absolute top-0 left-0 right-0 z-10 flex justify-center items-center pl-10 ">
                 <div className="flex items-center gap-2 text-white font-bold text-xl">
                     <a onClick={() => navigate("/login")}>Login</a>
@@ -13,9 +13,10 @@ export function HomePage() {
                 </div>
             </nav>
         <div className="grid grid-rows-3 grid-flow-col ">
+{/* Suggested code may be subject to a license. Learn more: ~LicenseLog:3724648974. */}
             <div className="row-span-3 ..."><SparklesPreview/></div>
             <div className="col-span-2 ..."><FlipWordsDemo /></div>
-            <div className="row-span-2 col-span-2 ...">03</div>
+            <div className="row-span-2 col-span-2 ..."><SparklesPreview /></div>
         </div>
             
             
